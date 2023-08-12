0377fc922537efc4060b4990fe1a45609a9e8f69
beczyczko <dbeczyczko@gmail.com>
2023-08-12 16:52:31 +0200

    refactor endpoints

---
+++ Dres.Extensions.DependencyInjection/ResourcesController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using System.Net.Mime;
+using System.Text;
+using Dres.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dres.Extensions.DependencyInjection;
+
+[ApiController]
+[Route("dres/resources")]
+public class ResourcesController : ControllerBase
+{
+    private readonly IResourcesProvider _resourcesProvider;
+    private readonly IAssembliesResolver _assembliesResolver;
+    private readonly IResourceRelationsPumlBuilder _resourceRelationsPumlBuilder;
+
+    public ResourcesController(
+        IResourcesProvider resourcesProvider,
+        IAssembliesResolver assembliesResolver,
+        IResourceRelationsPumlBuilder resourceRelationsPumlBuilder)
+    {
+        _resourcesProvider = resourcesProvider;
+        _assembliesResolver = assembliesResolver;
+        _resourceRelationsPumlBuilder = resourceRelationsPumlBuilder;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IImmutableList<Resource>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IImmutableList<Resource>>> All()
+    {
+        await Task.Yield();
+
+        var assemblies = _assembliesResolver.GetAvailable();
+        var resources = _resourcesProvider.Get(assemblies);
+        return Ok(resources);
+    }
+
+    [HttpGet("as-puml-file")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK, MediaTypeNames.Text.Plain)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<string>> AsPumlFile()
+    {
+        await Task.Yield();
+
+        var assemblies = _assembliesResolver.GetAvailable();
+        var resources = _resourcesProvider.Get(assemblies);
+
+        var puml = _resourceRelationsPumlBuilder.Build(resources);
+
+        return Ok(puml);
+    }
+
+    [HttpGet("download-puml-file")]
+    [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK, MediaTypeNames.Text.Plain)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> DownloadPumlFile()
+    {
+        await Task.Yield();
+
+        var assemblies = _assembliesResolver.GetAvailable();
+        var resources = _resourcesProvider.Get(assemblies);
+
+        var puml = _resourceRelationsPumlBuilder.Build(resources);
+
+        var pumlBytes = Encoding.UTF8.GetBytes(puml);
+
+        return File(pumlBytes, "text/plain", "dres.puml");
+    }
+}
--- Dres.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ Dres.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Immutable;
-using Dres.Core;
+﻿using Dres.Core;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dres.Extensions.DependencyInjection;
@@ -13,12 +11,10 @@ public static class ServiceCollectionExtensions
         var dresOptions = new DresOptions();
         options(dresOptions);
 
-        // Other service registrations
         services.AddTransient<IResourcesProvider, ResourcesProvider>();
         services.AddTransient<IResourceRelationsPumlBuilder, ResourceRelationsPumlBuilder>();
         services.AddSingleton<IAssembliesResolver>(_ => new AssembliesResolver(dresOptions.AssembliesGetFunc));
 
-        // Register the controllers from your library
         services.AddControllers().AddApplicationPart(typeof(ResourcesController).Assembly);
     }
 
@@ -29,31 +25,5 @@ public static class ServiceCollectionExtensions
         app.UseEndpoints(builder => builder.MapControllerRoute(
             name: "dres",
             pattern: "dres/{controller}"));
-    }
-}
-
-[ApiController]
-[Route("dres/[controller]/[action]")]
-public class ResourcesController : ControllerBase
-{
-    private readonly IResourcesProvider _resourcesProvider;
-    private readonly IAssembliesResolver _assembliesResolver;
-
-    public ResourcesController(
-        IResourcesProvider resourcesProvider,
-        IAssembliesResolver assembliesResolver)
-    {
-        _resourcesProvider = resourcesProvider;
-        _assembliesResolver = assembliesResolver;
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<IImmutableList<Resource>>> All()
-    {
-        await Task.Yield();
-
-        var assemblies = _assembliesResolver.GetAvailable();
-        var resources = _resourcesProvider.Get(assemblies);
-        return Ok(resources);
     }
 }
