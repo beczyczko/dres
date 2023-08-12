720b3e5dfc6586f1311531ba72f4815484e91241
beczyczko <dbeczyczko@gmail.com>
2023-08-12 16:02:59 +0200

    add testWebApp project

---
+++ Dres.TestWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dres.TestWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
