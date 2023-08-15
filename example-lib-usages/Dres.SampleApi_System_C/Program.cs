db6d964cd0500b0fcfaab7471c79c2fd4f9e5d3a
beczyczko <dbeczyczko@gmail.com>
2023-08-15 22:58:25 +0200

    add another sample project

---
+++ example-lib-usages/Dres.SampleApi_System_C/Program.cs
@@ -0,0 +1,61 @@
+using Dres.Extensions.ApplicationBuilder;
+using Dres.Extensions.DependencyInjection;
+using Test.Resources.Carts;
+using Test.Resources.Purchases;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDres(options =>
+{
+    options.AssembliesGetFunc = () => new[]
+    {
+        // only from System_A
+        typeof(Cart).Assembly,
+        typeof(Purchase).Assembly
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.UseDres();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
